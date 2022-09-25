dac6fe2ebda891c1b98b271bca9031091e6a9c2c
nadia <crocodile.who.wears.socks@yandex.ru>
2022-09-25 21:52:26 +0300

    fixed

--- mysite/info/views.py
+++ mysite/info/views.py
@@ -1,19 +1,6 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
 class HomePageView(TemplateView):
 
     template_name = "info/home.html"
-
-    def get(self, request, *args, **kwargs):
-        context = {'city': 'Казань',
-                   'fio': 'Тарасенко Надежда Александровна',
-                   'hobbies': ["рисование (в основном диджитал)",
-                               "аниме и книги (обычно фэнтези)",
-                               "программирование"],
-                   'experience': 'проекты (один индивидуальный и два групповых) '
-                                 'на втором курсе Лицея Академии Яндекса\n'
-                                 '(в том числе сайт на Flask)'
-                   }
-        return render(request, "info/home.html", context=context)
